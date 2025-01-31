e73f047177db34eaeb022b0c406d86bb0c17b770
Med-Amine-AB <mohamedamineaboudrar@gmail.com>
2025-01-31 16:32:53 +0100

    Project 2 (guess number): update the project to handel Value Errors bu the user

--- Project 2 (guess number).py
+++ Project 2 (guess number).py
@@ -1,15 +1,23 @@
 import random
 
-num = random.randint(1, 10)  # Generates a random integer between 1 and 10 (inclusive)
+num: int = random.randint(1, 10)  # Generates a random integer between 1 and 10 (inclusive)
+
+
 
 print("Guess a number between 1 to 10")
-user_num = int(input("Guess: "))
 
-while user_num != num:
+while True:
+    try:
+        user_num = int(input("Guess: "))
+    except ValueError :
+        print("Please insert an integer number!")
+        continue
+
+    if user_num == num:
+        print("You guessed it!")
+        break
+
     if user_num < num:
         print("Your number is Lower")
-        user_num = int(input("Guess: "))
     else:
         print("Your number is higher")
-        user_num = int(input("Guess: "))
-print("You guessed it!")
